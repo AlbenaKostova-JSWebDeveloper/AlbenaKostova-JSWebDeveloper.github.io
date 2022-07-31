2497281f8988365d37bd44e85ca9b24cb97e778d
Albena Kostova <albena.kostov@gmail.com>
2022-07-31 18:28:26 +0200

    Static signup method

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,9 +1,30 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
 
 const schema = new Schema({
     username: { type: String, required: true, unique: true }, 
     email: { type: String, required: true, unique: true },
     hashedPassword: { type: String, required: true },
 });
+
+// static signup method
+schema.statics.signup = async function (username, email, password) {
+    
+    
+    const emailExists = await this.findOne({ email });
+    const usernameExists = await this.findOne({ username });
+    
+    if (emailExists || usernameExists) {
+        throw new Error('Such user already exists');
+    }
+    
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    
+    const user = await this.create({ username, email, password: hash });
+    
+    return user;
+}
 
 module.exports = model('User', schema);
