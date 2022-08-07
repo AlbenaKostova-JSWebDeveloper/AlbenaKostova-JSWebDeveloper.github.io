1189abd5ded804e1ede2c73751bf98ecf420da4f
Albena Kostova <albena.kostov@gmail.com>
2022-08-07 20:01:56 +0200

    Responsive base layout

--- frontend/src/pages/dashboard/Dashboard.js
+++ frontend/src/pages/dashboard/Dashboard.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import './Dashboard.scss';
+
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className='container dashboard pt-3'>Dashboard</div>
   )
 }
