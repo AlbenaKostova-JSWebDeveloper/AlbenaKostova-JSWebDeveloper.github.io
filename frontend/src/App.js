91bbf406fa29ed2ada6311b73daab196b29a2002
Albena Kostova <albena.kostov@gmail.com>
2022-08-09 13:06:40 +0200

    Suspense lazy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -6,6 +6,7 @@ import Navbar from './components/navbar/Navbar.js';
 import Footer from './components/footer/Footer.js';
 import './App.scss';
 
+const Loading = React.lazy(() => import('./components/loading/Loading.js'));
 const Dashboard = React.lazy(() => import('./pages/dashboard/Dashboard.js'));
 const Projects = React.lazy(() => import('./pages/projects/Projects.js'));
 const Docs = React.lazy(() => import('./pages/docs/Docs.js'));
@@ -21,12 +22,14 @@ function App() {
                     <Navbar />
                 </header>
                 <main className="App-main pages pt-3">
-                    <Routes>
-                        <Route path='/' element={<Dashboard />}/>
-                        <Route path='/projects' element={<Projects />}/>
-                        <Route path='/documents' element={<Docs />}/>
-                        <Route path='/contact' element={<Contact />}/>
-                    </Routes>
+                    <Suspense fallback={<Loading />}>
+                        <Routes>
+                            <Route path='/' element={<Dashboard />}/>
+                            <Route path='/projects' element={<Projects />}/>
+                            <Route path='/documents' element={<Docs />}/>
+                            <Route path='/contact' element={<Contact />}/>
+                        </Routes>
+                    </Suspense>
                 </main>
                 <footer className="App-footer">
                     <Footer />
