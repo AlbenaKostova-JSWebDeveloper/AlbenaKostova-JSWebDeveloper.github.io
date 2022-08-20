3b1558922fe3868b32a59123de3747f2f04518f6
Albena Kostova <albena.kostov@gmail.com>
2022-08-20 16:35:53 +0200

    Styling projects

--- frontend/src/pages/projects/Projects.js
+++ frontend/src/pages/projects/Projects.js
@@ -1,8 +1,8 @@
-import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Suspense, useEffect } from 'react';
 
 import { useProjectsContext } from '../../hooks/useProjectsContext';
 
+import Loading from '../../components/loading/Loading';
 import Card from '../../components/card/Card';
 import './Projects.scss';
 
@@ -28,18 +28,33 @@ export default function Projects() {
             <h3 className="section-title">Projects</h3>
             
             <section className="projects grid">
+                <Suspense fallback={<Loading />}>
                 {projects && projects.map((project) => (
                     <Card key={project._id} project={project}>
                         <article className='project-card'>
-                            <Link className="link" to={ project.link }>
+                            <div className="link">
                                 <header className="image-container">
                                     <img src={ project.image } alt="screenshot of the website" className="image" />
                                 </header>
-                                <h5 className="project-title">{ project.title }</h5>        
-                            </Link>
+                                <h5 className="project-title">{ project.title }</h5> 
+                                <p className="p description">{ project.description }</p>
+                                <p className="p technologies">
+                                    <span className="tip">Technologies used: </span>
+                                    { project.technologies.join(' | ') }
+                                </p>
+                                <p className="p repo">
+                                    <span className="tip">View code: </span>
+                                    <a href={ project.link } target="_blank" rel="noopener noreferrer" className="url">{ project.title }</a>
+                                </p>
+                                <p className="p app">
+                                    <span className="tip">Visit App: </span>
+                                    <a href={ project.link } target="_blank" rel="noopener noreferrer" className="url">{ project.title }</a>
+                                </p>                                       
+                            </div>
                         </article>
                     </Card>
-                ))}                
+                ))}
+                </Suspense>              
             </section>
         </div>
     );
