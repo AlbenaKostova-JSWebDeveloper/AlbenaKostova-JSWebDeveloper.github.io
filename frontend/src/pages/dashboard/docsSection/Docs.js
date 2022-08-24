52c2921e6f123b6596c33cedb3729890a861af62
Albena Kostova <albena.kostov@gmail.com>
2022-08-24 18:03:38 +0200

    Dashboard section docs

--- frontend/src/pages/dashboard/docsSection/Docs.js
+++ frontend/src/pages/dashboard/docsSection/Docs.js
@@ -4,13 +4,22 @@ import './Docs.scss';
 
 export default function Docs() {
     return (
-        <section className="docs">
+        <section className="documents-section">
             <h3 className="section-title">Documents</h3>
             
-            <Suspense fallback={<div>Loading...</div>}>
-
-            </Suspense>
-
+            <article className="documents">
+                <Suspense fallback={<div>Loading...</div>}>
+                <div className="icon-container">
+                    <i className="icon fa-solid fa-award fa-1x" />                    
+                </div>
+                <div className="icon-container">
+                    <i class="icon fa-solid fa-user fa-1x"></i>
+                </div>
+                <div className="icon-container">
+                    <i className="icon fa-solid fa-file-arrow-down fa-1x" />
+                </div>
+                </Suspense>                
+            </article>
         </section>
     );
 }
